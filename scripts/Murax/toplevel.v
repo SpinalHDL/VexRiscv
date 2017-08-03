d962406b26c4de9e64742a9745db99d73b3bbf99
Charles Papon <charles.papon.90@gmail.com>
2017-08-03 23:22:57 +0200

    scripts/murax better makefile, add pullup on jtag interface

--- scripts/Murax/toplevel.v
+++ scripts/Murax/toplevel.v
@@ -14,28 +14,61 @@ module toplevel(
   wire [31:0] io_gpioA_read;
   wire [31:0] io_gpioA_write;
   wire [31:0] io_gpioA_writeEnable;
-  wire io_mainClk;
+
+
   wire io_jtag_tck;
-  
+  wire io_jtag_tdi;
+  wire io_jtag_tms;
+
+  SB_IO #(
+      .PIN_TYPE(6'b0000_01),
+      .PULLUP(1'b1)
+  ) io_jtag_tck_buff (
+      .PACKAGE_PIN(io_H16),
+      .D_IN_0(io_jtag_tck)
+  );
+
+  SB_IO #(
+      .PIN_TYPE(6'b0000_01),
+      .PULLUP(1'b1)
+  ) io_jtag_tdi_buff (
+      .PACKAGE_PIN(io_G15),
+      .D_IN_0(io_jtag_tdi)
+  );
+
+  SB_IO #(
+      .PIN_TYPE(6'b0000_01),
+      .PULLUP(1'b1)
+  ) io_jtag_tms_buff (
+      .PACKAGE_PIN(io_F15),
+      .D_IN_0(io_jtag_tms)
+  );
+
+
+  wire io_mainClk_gb;
+  wire io_jtag_tck_gb;
+
   SB_GB mainClkBuffer (
     .USER_SIGNAL_TO_GLOBAL_BUFFER (io_J3),
-    .GLOBAL_BUFFER_OUTPUT ( io_mainClk)
+    .GLOBAL_BUFFER_OUTPUT ( io_mainClk_gb)
   );
-    
+
   SB_GB jtagClkBuffer (
-    .USER_SIGNAL_TO_GLOBAL_BUFFER (io_H16),
-    .GLOBAL_BUFFER_OUTPUT ( io_jtag_tck)
+    .USER_SIGNAL_TO_GLOBAL_BUFFER (io_jtag_tck),
+    .GLOBAL_BUFFER_OUTPUT ( io_jtag_tck_gb)
   );
-  
+
+
+
   assign io_led = io_gpioA_write[7 : 0];
 
   Murax murax ( 
     .io_asyncReset(0),
-    .io_mainClk (io_mainClk ),
-    .io_jtag_tck(io_jtag_tck),
-    .io_jtag_tdi(io_G15),
-    .io_jtag_tdo(io_G16),
-    .io_jtag_tms(io_F15),
+    .io_mainClk (io_mainClk_gb ),
+    .io_jtag_tck(io_jtag_tck_gb),
+    .io_jtag_tdi(io_jtag_tdi),
+    .io_jtag_tdo(io_jtag_tdo),
+    .io_jtag_tms(io_jtag_tms),
     .io_gpioA_read       (io_gpioA_read),
     .io_gpioA_write      (io_gpioA_write),
     .io_gpioA_writeEnable(io_gpioA_writeEnable),
