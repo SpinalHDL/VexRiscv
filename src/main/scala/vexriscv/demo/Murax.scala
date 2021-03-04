caf1bde49b643939cb2c717dbcae450a4f1ca58b
Dolu1990 <charles.papon.90@gmail.com>
2021-03-04 10:16:45 +0100

    Add MuraxAsicBlackBox example

--- src/main/scala/vexriscv/demo/Murax.scala
+++ src/main/scala/vexriscv/demo/Murax.scala
@@ -483,3 +483,13 @@ object Murax_arty{
     SpinalVerilog(Murax(MuraxConfig.default(false).copy(coreFrequency = 100 MHz,onChipRamSize = 32 kB, onChipRamHexFile = hex)))
   }
 }
+
+
+object MuraxAsicBlackBox{
+  def main(args: Array[String]) {
+    println("Warning this soc do not has any rom to boot on.")
+    val config = SpinalConfig()
+    config.addStandardMemBlackboxing(blackboxAll)
+    config.generateVerilog(Murax(MuraxConfig.default(false).copy(coreFrequency = 100 MHz,onChipRamSize = 32 kB)))
+  }
+}
