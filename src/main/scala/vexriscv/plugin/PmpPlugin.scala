df03c99ab2a851c612d945afcc7165a8898b282c
occheung <dc@m-labs.hk>
2021-10-19 11:39:25 +0800

    pmp_setter: fix mask generation

--- src/main/scala/vexriscv/plugin/PmpPlugin.scala
+++ src/main/scala/vexriscv/plugin/PmpPlugin.scala
@@ -90,7 +90,7 @@ class PmpSetter(cutoff : Int) extends Component with Pmp {
 
   val ones = io.addr & ~(io.addr + 1)
   io.base := io.addr(xlen - 3 downto cutoff - 2) ^ ones(xlen - 3 downto cutoff - 2)
-  io.mask := ~ones(xlen - 2 downto cutoff - 1)
+  io.mask := ~(ones(xlen - 4 downto cutoff - 2) @@ U"1")
 }
 
 case class ProtectedMemoryTranslatorPort(bus : MemoryTranslatorBus)
