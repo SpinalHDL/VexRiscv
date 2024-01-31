c06d7f5cb4bfb5c4c54c039135f1a78d8c317308
Jacob Tilger <jacob.2015@tilger.net>
2024-01-31 17:12:42 +0100

    renamed utime to time & doc

--- src/main/scala/vexriscv/plugin/CounterPlugin.scala
+++ src/main/scala/vexriscv/plugin/CounterPlugin.scala
@@ -106,7 +106,7 @@ class CounterPlugin(config : CounterPluginConfig) extends Plugin[VexRiscv] with 
     import pipeline._
     import pipeline.config._
 
-    if (utimeAccess != CsrAccess.NONE) time = in UInt(64 bits) setName("utime")
+    if (utimeAccess != CsrAccess.NONE) time = in UInt(64 bits) setName("time")
   }
 
   override def build(pipeline : VexRiscv) : Unit = {
